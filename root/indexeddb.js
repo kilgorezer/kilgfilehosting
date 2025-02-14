85b8e1fa4682ea600964218b808bdbb60eba88bd
kilgorezer <74324783+kilgorezer@users.noreply.github.com>
2025-02-14 00:37:50 -0800

    Update indexeddb.js

--- root/indexeddb.js
+++ root/indexeddb.js
@@ -1,41 +1,48 @@
-(function(Scratch) {
-  'use strict';
+// Name: IndexedDB
+// ID: indexeddb
+// Description: Adds basic IndexedDB functionality
+// By: kilgorezer <https://scratch.mit.edu/users/kilgorezer/>
+// License: MPL-2.0
 
-  const prefix = 'idb';
+(function (Scratch) {
+  "use strict";
+
+  const prefix = "idb";
 
   class DatabaseManager {
     openDatabase(dbName) {
       return new Promise((resolve, reject) => {
         const request = indexedDB.open(dbName, 1); // Fixed version 1
 
-        request.onerror = event => reject(event.target.error);
+        request.onerror = (event) => reject(event.target.error);
 
-        request.onsuccess = event => resolve(event.target.result);
+        request.onsuccess = (event) => resolve(event.target.result);
 
-        request.onupgradeneeded = event => {
+        request.onupgradeneeded = (event) => {
           const db = event.target.result;
           // Create the *single, fixed* object store here.
-          if (!db.objectStoreNames.contains('myStore')) { // Fixed store name: 'myStore'
-            db.createObjectStore('myStore', { keyPath: 'id' });
+          if (!db.objectStoreNames.contains("myStore")) {
+            // Fixed store name: 'myStore'
+            db.createObjectStore("myStore", { keyPath: "id" });
           }
         };
       });
     }
 
-
     deleteDatabase(dbName) {
       return new Promise((resolve, reject) => {
         const request = indexedDB.deleteDatabase(dbName);
-        request.onerror = event => reject(event.target.error);
+        request.onerror = (event) => reject(event.target.error);
         request.onsuccess = () => resolve();
       });
     }
 
-    putValue(dbName, key, value) { // Simplified: dbName, key, value
-      return this.openDatabase(dbName).then(db => {
+    putValue(dbName, key, value) {
+      // Simplified: dbName, key, value
+      return this.openDatabase(dbName).then((db) => {
         return new Promise((resolve, reject) => {
-          const transaction = db.transaction(['myStore'], 'readwrite'); // Fixed store name
-          const objectStore = transaction.objectStore('myStore'); // Fixed store name
+          const transaction = db.transaction(["myStore"], "readwrite"); // Fixed store name
+          const objectStore = transaction.objectStore("myStore"); // Fixed store name
           const request = objectStore.put({ id: key, value: value });
 
           request.onsuccess = () => resolve();
@@ -44,24 +51,26 @@
       });
     }
 
-    getValue(dbName, key) { // Simplified: dbName, key
-      return this.openDatabase(dbName).then(db => {
+    getValue(dbName, key) {
+      // Simplified: dbName, key
+      return this.openDatabase(dbName).then((db) => {
         return new Promise((resolve, reject) => {
-          const transaction = db.transaction(['myStore'], 'readonly'); // Fixed store name
-          const objectStore = transaction.objectStore('myStore'); // Fixed store name
+          const transaction = db.transaction(["myStore"], "readonly"); // Fixed store name
+          const objectStore = transaction.objectStore("myStore"); // Fixed store name
           const request = objectStore.get(key);
 
-          request.onsuccess = event => resolve(event.target.result ? event.target.result.value : null);
+          request.onsuccess = (event) =>
+            resolve(event.target.result ? event.target.result.value : '');
           request.onerror = () => reject();
         });
       });
     }
 
     deleteValue(dbName, key) {
-      return this.openDatabase(dbName).then(db => {
+      return this.openDatabase(dbName).then((db) => {
         return new Promise((resolve, reject) => {
-          const transaction = db.transaction(['myStore'], 'readwrite');
-          const objectStore = transaction.objectStore('myStore');
+          const transaction = db.transaction(["myStore"], "readwrite");
+          const objectStore = transaction.objectStore("myStore");
           const request = objectStore.delete(key); // Use delete() method
 
           request.onsuccess = () => resolve();
@@ -78,90 +87,112 @@
 
     getInfo() {
       return {
-        id: 'indexeddb',
-        name: 'IndexedDB',
-        color1: '#4285F4',
+        id: "indexeddb",
+        name: "IndexedDB",
+        color1: "#4285F4",
         blocks: [
           {
-            opcode: 'openDatabase',
+            opcode: "openDatabase",
             blockType: Scratch.BlockType.COMMAND,
             hideFromPalette: true,
-            text: 'Open/Create Database [NAME]',
+            text: "Open/Create Database [NAME]",
             arguments: {
-              NAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'myDatabase' }
-            }
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "myDatabase",
+              },
+            },
           },
           {
-            opcode: 'deleteDatabase',
+            opcode: "deleteDatabase",
             blockType: Scratch.BlockType.COMMAND,
-            text: 'Delete Database [NAME]',
+            text: "Delete Database [NAME]",
             arguments: {
-              NAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'myDatabase' }
-            }
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "myDatabase",
+              },
+            },
           },
           {
-            opcode: 'putValue',
+            opcode: "putValue",
             blockType: Scratch.BlockType.COMMAND,
-            text: 'Put [KEY] : [VALUE] in Database [DB_NAME]',
+            text: "Put [KEY] : [VALUE] in Database [DB_NAME]",
             arguments: {
               KEY: { type: Scratch.ArgumentType.STRING },
               VALUE: { type: Scratch.ArgumentType.STRING },
-              DB_NAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'myDatabase' }
-            }
+              DB_NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "myDatabase",
+              },
+            },
           },
           {
-            opcode: 'getValue',
+            opcode: "getValue",
             blockType: Scratch.BlockType.REPORTER,
-            text: 'Get [KEY] from Database [DB_NAME]',
+            text: "Get [KEY] from Database [DB_NAME]",
             arguments: {
               KEY: { type: Scratch.ArgumentType.STRING },
-              DB_NAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'myDatabase' }
-            }
+              DB_NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "myDatabase",
+              },
+            },
           },
           {
-            opcode: 'deleteValue',
+            opcode: "deleteValue",
             blockType: Scratch.BlockType.COMMAND,
-            text: 'Delete [KEY] from Database [DB_NAME]',
+            text: "Delete [KEY] from Database [DB_NAME]",
             arguments: {
               KEY: { type: Scratch.ArgumentType.STRING },
-              DB_NAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'myDatabase' }
-            }
+              DB_NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "myDatabase",
+              },
+            },
           },
           {
-            opcode: 'clearall',
+            opcode: "clearall",
             blockType: Scratch.BlockType.COMMAND,
-            text: 'Clear all keys from Database [DB_NAME]',
+            text: "Clear all keys from Database [DB_NAME]",
             arguments: {
-              DB_NAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'myDatabase' }
-            }
-          }
-        ]
+              DB_NAME: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "myDatabase",
+              },
+            },
+          },
+        ],
       };
     }
 
     async openDatabase(args) {
-      (await this.dbManager.openDatabase(prefix+'_'+args.NAME)).close();
+      (await this.dbManager.openDatabase(prefix + "_" + args.NAME)).close();
     }
 
     async deleteDatabase(args) {
-      await this.dbManager.deleteDatabase(prefix+'_'+args.NAME);
+      await this.dbManager.deleteDatabase(prefix + "_" + args.NAME);
     }
 
     putValue(args) {
-      this.dbManager.putValue(prefix+'_'+args.DB_NAME, args.KEY, args.VALUE);
+      this.dbManager.putValue(
+        prefix + "_" + args.DB_NAME,
+        args.KEY,
+        args.VALUE,
+      );
     }
 
     getValue(args) {
-      return this.dbManager.getValue(prefix+'_'+args.DB_NAME, args.KEY);
+      return this.dbManager.getValue(prefix + "_" + args.DB_NAME, args.KEY);
     }
 
     deleteValue(args) {
-      this.dbManager.deleteValue(prefix+args.DB_NAME, args.KEY);
+      this.dbManager.deleteValue(prefix + args.DB_NAME, args.KEY);
     }
 
     async clearall(args) {
-      await this.dbManager.deleteDatabase(prefix+args.DB_NAME);
-      (await this.dbManager.openDatabase(prefix+args.DB_NAME)).close();
+      await this.dbManager.deleteDatabase(prefix + args.DB_NAME);
+      (await this.dbManager.openDatabase(prefix + args.DB_NAME)).close();
     }
   }
 
